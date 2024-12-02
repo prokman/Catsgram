69a8a367e38ab94a16882f43b4afae4124136b16
Vyacheslav <prokman@ya.ru>
2024-12-02 22:13:26 +0600

    First Commit

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.*;
+
+import java.time.Instant;
+
+@Data
+@EqualsAndHashCode(of = {"email"})
+public class User {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Instant registrationDate;
+}
